9dd3e3687731995b982a2b12e7e9eca08962b631
Nitin-web-dev <nitinwebdev5@gmail.com>
2024-05-27 12:10:55 +0530

    addead asset img

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4} from 'uuid'
 import generateJoke from "./generateJoke";
 import "./styles/main.scss"
+import laughing from  './assets/laughing.svg'
 
 let joke = generateJoke();
 
@@ -10,4 +11,8 @@ console.log(joke)
 // third party module
 let id = uuidv4();
 console.log('id: ', id);
+
+let jokeEl = document.getElementById('IMG');
+
+jokeEl.setAttribute('src', laughing);
 
