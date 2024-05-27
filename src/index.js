73d45e854d379a3f216e80639d47df3704a19d76
Nitin-web-dev <nitinwebdev5@gmail.com>
2024-05-27 10:26:07 +0530

    addead uuid library third party lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import { v4 as uuidv4} from 'uuid'
 import generateJoke from "./generateJoke";
-
 
 let joke = generateJoke();
 
 console.log(joke)
+
+
+// third party module
+let id = uuidv4();
+console.log('id: ', id);
